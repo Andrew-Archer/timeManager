cf5be3457c4aef842c84de9559343b71ea76cd89
Andrew-Archer <razandale@gmail.com>
2017-02-27 07:05:38 +0400

    one more time.

--- timeManager/src/main/java/timemanager/TimeManager.java
+++ timeManager/src/main/java/timemanager/TimeManager.java
@@ -1,80 +1,81 @@
 package timemanager;
 
-
-
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import timemanager.actors.Person;
+import timemanager.actors.Worker;
+import timemanager.cellSplitLogic.CellSplitLogic;
+import timemanager.exceptions.EndBeforeStartException;
+import timemanager.exceptions.ZeroLengthException;
+import timemanager.validation.PeriodValidator;
 
 public class TimeManager {
+
     //For example one hour
-    private final int minimnPeriodOfWorkInHours = 1;
+    private final int minimunPeriodOfWorkInHours = 1;
     //For example hours in a day
-    private final int subItervalLength = minimnPeriodOfWorkInHours * 8;
+    private final int subItervalLength = minimunPeriodOfWorkInHours * 8;
     private List<TimeCell> timeCellsAvailable = new ArrayList<>();
-    private List<TimeCell> timeCellsRequest = new ArrayList<>();
+    private List<TimeCell> workRequestTimeCell = new ArrayList<>();
     private final List<TimeCell> actualWorkGraph = new ArrayList<>();
 
     /**
      * Calculates List of workers who sent queries for work. Searches for
      * workers in workersTImeWishList.
-     *
-     * @param start
-     * @param end
-     * @return List of workers who sent queries for work in a given period.
+     * @param start start point of search TimeCell assigned to executors.
+     * @param end end point of search.
+     * @return List of workers who sent queries for to work in a given period.
      */
-    /*public List<Worker> getWorkersAvailableInPeriod(LocalDateTime start, LocalDateTime end) {
+    public List<Worker> getWorkersAvailableInPeriod(LocalDateTime start, LocalDateTime end) {
         List<Worker> workersList = new ArrayList<>();
-        
-        for (TimeCell timeCellRequest : timeCellsRequest){
-            if (timeCellRequest.isIncludedIn(start.minusNanos(1), end.plusNanos(1)) &&
-                   !workersList.contains(timeCellRequest.getWorker())){
-                workersList.add(timeCellRequest.getWorker());
+        for (TimeCell workRequest : workRequestTimeCell) {
+            if (workRequest.isIncludedIn(start.minusNanos(1), end.plusNanos(1))
+                    && !workersList.contains(workRequest.getExecutor())) {
+                workersList.add(workRequest.getExecutor());
             }
         }
         return workersList;
     }
 
-    public void addTimeCellToActualWorkGraph(TimeCell aTimeCell){
-        List<TimeCell> originalList = new ArrayList<>();
-        List<TimeCell> listToPushIntoActualGraph = new ArrayList<>();
-        if (actualWorkGraph.isEmpty()){
-            actualWorkGraph = getFairGraphOfWork(LocalDateTime.MAX, LocalDateTime.MIN);
-        }
-        
-        originalList = aTimeCell.getOverlapingTimeCells(actualWorkGraph);
-        
-        for (TimeCell timeCell : originalList){
-            
-        }
-        
-    }
-    
+
     /**
+     *
+     *
      *
      * @param start
      * @param end
-     * @return 
+     * @param workersList
+     * @param creator
+     * @return
+     * @throws timemanager.exceptions.ZeroLengthException
      */
-    /*public List<TimeCellOfJob> getFairGraphOfWork(LocalDateTime start, LocalDateTime end) throws CloneNotSupportedException {
-        final List<Worker> workersList = getWorkersAvailableInPeriod(start, end);
-        
+
+    public List<TimeCell> getFairGraphOfWork(
+            LocalDateTime start,
+            LocalDateTime end,
+            List<Worker> workersList,
+            Person creator) throws ZeroLengthException {
         //To hold fair graph of work
-        List<TimeCellOfJob> fairGraphOfWork = new ArrayList<>();
-        
+        List<TimeCell> fairGraphOfWork = new ArrayList<>();
+
         //number of hours in the interval between start and end
         long numberOfHours = Duration.between(start, end).toHours();
 
         //Fills up fair graph of work
-        for (LocalDateTime i = start; i.isBefore(end); i.plusHours(minimnPeriodOfWorkInHours)){
+        for (LocalDateTime i = start; i.isBefore(end); i.plusHours(minimunPeriodOfWorkInHours)){
             for (Worker worker: workersList){
                 try {
-                    fairGraphOfWork.add(new TimeCellOfJob(
+                    fairGraphOfWork.add(new TimeCell(
                             start,
-                            start.plusHours(minimnPeriodOfWorkInHours),
+                            start.plusHours(minimunPeriodOfWorkInHours),
                             LocalDateTime.now(),
+                            creator,
                             null,
-                            TypeOfWork.ANY,
-                            worker));
+                            TypeOfWork.ANY));
                 } catch (EndBeforeStartException ex) {
                     Logger.getLogger(TimeManager.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (ZeroLengthException ex) {
@@ -82,34 +83,108 @@ public class TimeManager {
                 }
             }
         }
+
         return fairGraphOfWork;
-    }
-    
-    public List<TimeCellOfJob> getActualGraphOfWork(
-            LocalDateTime start,
-            LocalDateTime end) throws
-            CloneNotSupportedException{
-        List<TimeCellOfJob> fairGraphOfWork = getFairGraphOfWork(start, end);
-        return null;
+
     }
 
-    public void addTimeCellAvailable(TimeCellOfJob newJob) {
-        //TODO
+    
+
+    public List<TimeCellOfJob> getActualGraphOfWork(
+
+            LocalDateTime start,
+
+            LocalDateTime end) throws
+
+            CloneNotSupportedException{
+
+        List<TimeCellOfJob> fairGraphOfWork = getFairGraphOfWork(start, end);
+
+        return null;
+
     }
+
+
+
+    public void addTimeCellAvailable(TimeCellOfJob newJob) {
+
+        //TODO
+
+    }
+
+
 
     public void removeTimeCellAvailable(TimeCellOfJob aJob) {
 
+
+
     }
 
+
+
     public void addTimeCellRequest(TimeCellOfWorkerTime timeCellRequest) {
+
         timeCellsRequest.add(timeCellRequest);
+
     }
+
+
 
     public void removeTimeCellRequest(TimeCellOfWorkerTime timeCellRequest) {
 
+
+
     }
+
+
 
     public void organizeTimeCells() {
 
+
+
     }*/
+    /**
+     *
+     * @param graphToInsertTo
+     * @param aCellToInsert
+     * @param aValidator
+     * @param splitLogic
+     * @return
+     * @throws Exception
+     */
+    public List<TimeCell> splitTimeCells(
+            List<TimeCell> graphToInsertTo,
+            TimeCell aCellToInsert,
+            PeriodValidator aValidator,
+            CellSplitLogic splitLogic,
+            CellSplitLogic splitLogicForPushed) throws
+            Exception {
+
+        //Get overlapping with the aCellToInsert cells from the graphToInsertTo.
+        List<TimeCell> overlappingOfGraphAndToInsertCell;
+        overlappingOfGraphAndToInsertCell = aCellToInsert.getOverlappingTimeCells(graphToInsertTo);
+
+        //Here we put splitted parts to return.
+        TimeCellSpliterationResult result = new TimeCellSpliterationResult();
+
+        //Initializing insertionLeft with aCellToInsert
+        result.setInsertionLeft(aCellToInsert);
+
+        //Inserting TimeCell to insert
+        for (TimeCell aTimeCell : overlappingOfGraphAndToInsertCell) {
+            result.add(splitLogic.split(result.getInsertionLeft(), aTimeCell));
+        }
+        //Inserting pushed out TimeCells
+        //Get TimeCell with the same name as.
+        List<TimeCell> timeCellsListForGivenWorker;
+        timeCellsListForGivenWorker = aCellToInsert.getTimeCellsAssignedTo(
+                graphToInsertTo,
+                aCellToInsert.getExecutor());
+        for (TimeCell pushedOut : result.getPushedOut()) {
+            for (TimeCell assinedForWorker : timeCellsListForGivenWorker) {
+                result.add(splitLogicForPushed.split(pushedOut, assinedForWorker));
+            }
+        }
+        return result.getToInsert();
+    }
 }
